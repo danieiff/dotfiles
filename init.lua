a88df5d5ae69310102f4dc6a5d6f06cce4d89922
danieiff <sugielff@gmail.com>
2023-06-07 23:56:30 +0900

    nvim snippet(lua-snip), completion(nvim-cmp)

--- init.lua
+++ init.lua
@@ -348,7 +348,96 @@ require 'nvim-autopairs'.setup()
 --K('n', '<leader>a', '<cmd>AerialToggle<CR>')
 
 vim.cmd("sign define LspDiagnosticsSignWarning texthl=LspDiagnosticsSignWarning numhl=LspDiagnosticsLineNrWarning")
+local luasnip = require 'luasnip'
+require 'luasnip.loaders.from_vscode'.lazy_load()
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
+local cmp = require 'cmp'
+cmp.setup({
+  snippet = { -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    -- C-b (back) C-f (forward) for snippet placeholder navigation.
+    --['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    --['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    -- ['<CR>'] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehaviour.Replace }), -- Set `select` to `false` to only confirm explicitly selected items.
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    -- { name = 'vsnip' }, -- For vsnip users.
+    { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline(':', {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = cmp.config.sources({
+--     { name = 'path' }
+--   }, {
+--     { name = 'cmdline' }
+--   })
+-- })
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- :LspInfo TODO: nvim-lspconfig source code
 --vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
 --  update_in_insert = false,
@@ -452,12 +541,14 @@ require 'lspconfig'.lua_ls.setup {
   },
 }
 
-require 'lspconfig'.bashls.setup { }
-
+require 'lspconfig'.bashls.setup { capabilities }
 local jsonls_capabilities = vim.lsp.protocol.make_client_capabilities()
 jsonls_capabilities.textDocument.completion.completionItem.snippetSupport = true
-require  'lspconfig'.jsonls.setup { 
-  capabilities = jsonls_capabilities,
+-- print(vim.inspect(jsonls_capabilities.textDocument.completion))
+-- print(vim.inspect(capabilities))
+-- print(vim.tbl_deep_extend('error',jsonls_capabilities, capabilities))
+require 'lspconfig'.jsonls.setup {
+  capabilities = jsonls_capabilities, --vim.tbl_deep_extend('error',jsonls_capabilities, capabilities),
   settings = {
     json = {
       schemas = require 'schemastore'.json.schemas(),
@@ -465,12 +556,14 @@ require  'lspconfig'.jsonls.setup {
     }
   }
 }
+
 require 'lspconfig'.yamlls.setup {
   settings = {
     yaml = {
       schemas = require('schemastore').yaml.schemas(),
     },
   },
+  capabilities
 }
 require 'lspconfig'.tsserver.setup { }
 require 'lspconfig'.tailwindcss.setup {
@@ -484,7 +577,8 @@ require 'lspconfig'.tailwindcss.setup {
       -- tailwindcss lang server wai swap = {
       vim.lsp.buf_notify(bufnr, "tailwindcss/getConfigurationResponse", { _id = params._id })
     end
-  }
+  },
+  capabilities
 
 }
 
@@ -498,9 +592,10 @@ rt.setup({
       vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
     end,
   },
+  capabilities
 })
 
-require 'lspconfig'.gopls.setup {}
+require 'lspconfig'.gopls.setup { capabilities }
 
 --require'lspconfig'.textlsp.setup {}
 
