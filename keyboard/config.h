064f76af3748bdb5ab3083df9bb236b7f7489da5
danieiff <sugielff@gmail.com>
2023-06-10 23:20:55 +0900

    feat(keyboard): ergodash setting

---
+++ keyboard/config.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define SELECT_SOFT_SERIAL_SPEED 1
+/*Sets the protocol speed when using serial communication*/
+//Speeds:
+//0: about 189kbps (Experimental only)
+//1: about 137kbps (default)
+//2: about 75kbps
+//3: about 39kbps
+//4: about 26kbps
+//5: about 20kbps
+
+#define TAPPING_TERM 120
+
+#define MASTER_LEFT
+
+#define AUTO_SHIFT_MODIFIERS
+#define AUTO_SHIFT_TIMEOUT 185
+
+#define ONESHOT_TAP_TOGGLE 3
+#define ONESHOT_TIMEOUT 5000
+
+#define NO_DEBUG
+#define NO_PRINT
---
+++ keyboard/keymap.c
@@ -0,0 +1,156 @@
+#include QMK_KEYBOARD_H
+
+enum layer_names {
+    _QWERTY,
+    // _LOWER,
+    // _RAISE,
+    _ADJUST
+};
+
+// enum custom_keycodes {
+//   QWERTY = SAFE_RANGE,
+//   LOWER,
+//   RAISE,
+//   ADJUST,
+// };
+
+#define EISU LALT(KC_GRV)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  /* Qwerty
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   * | ESC  |   1  |   2  |   3  |   4  |   5  |  Del |                    | Bksp |   6  |   7  |   8  |   9  |   0  |Pscree|
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * |  `   |   Q  |   W  |   E  |   R  |   T  |   [  |                    |   ]  |   Y  |   U  |   I  |   O  |   P  |  \   |
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * | Tab  |   A  |   S  |   D  |   F  |   G  |   -  |                    |   =  |   H  |   J  |   K  |   L  |   ;  |  "   |
+   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  | Space|                    | Enter|   N  |   M  |   ,  |   .  |   /  | Shift|
+   * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
+   * | Ctrl |  GUI |  ALt | EISU |||||||| Lower| Space|  Del |||||||| Bksp | Enter| Raise|||||||| Left | Down |  Up  | Right|
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   */
+  [_QWERTY] = LAYOUT(
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,                        _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
+   KC_PSCR,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS,                        KC_EQL , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+   KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_VOLU,                        KC_DEL , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MUTE,
+   _______, KC_LGUI, KC_LALT,  EISU,          _______,    KC_VOLD, KC_SPC,        KC_ENT, KC_BSPC, OSL(_ADJUST),     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  ),
+
+  /* Lower
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |   {  |                    |   }  |  F6  |  F7  |  F8  |  F9  |  F10 |  F12 |
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * |  ~   |   !  |   @  |   #  |   $  |   %  |   _  |                    |   +  |   ^  |   &  |   *  |   (  |   )  |  |   |
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * | Tab  |   1  |   2  |   3  |   4  |   5  |  Del |                    | Bksp |   H  |   J  |   K  |   L  |   :  |  "   |
+   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+   * | Shift|   6  |   7  |   8  |   9  |   0  | Space|                    | Enter|   N  |   M  |   <  |   >  |   ?  | Shift|
+   * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
+   * | Ctrl |  GUI |  ALt | EISU |||||||| Lower| Space|  Del |||||||| Bksp | Enter| Raise|||||||| Home |PageDn|PageUp|  End |
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   */
+  // [_LOWER] = LAYOUT(
+  //   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                        KC_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
+  //   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+  //   KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL ,                        KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQT ,
+  //   KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT,
+  //   KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC ,KC_DEL,         KC_BSPC,KC_ENT , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END
+  // ),
+
+  /* Raise
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |   {  |                    |   }  |  F6  |  F7  |  F8  |  F9  |  F10 |  F12 |
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * |  ~   |   !  |   @  |   #  |   $  |   %  |   _  |                    |   +  |   ^  |   &  |   *  |   (  |   )  |  |   |
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * | Tab  |   1  |   2  |   3  |   4  |   5  |  Del |                    | Bksp |   H  |   J  |   K  |   L  |   :  |  "   |
+   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+   * | Shift|   6  |   7  |   8  |   9  |   0  | Space|                    | Enter|   N  |   M  |   <  |   >  |   ?  | Shift|
+   * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
+   * | Ctrl |  GUI |  ALt | EISU |||||||| Lower| Space|  Del |||||||| Bksp | Enter| Raise|||||||| Home |PageDn|PageUp|  End |
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   */
+  // [_RAISE] = LAYOUT(
+    // KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                        KC_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
+    // KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+    // KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL ,                        KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQT ,
+    // KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT,
+    // KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC ,KC_DEL,         KC_BSPC,KC_ENT , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END
+  // ),
+
+  /* Adjust
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
+   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+   * |      | Reset|RGB ON|  MODE|  HUE-|  HUE+|      |                    |      |  SAT-|  SAT+|  VAL-|  VAL+|      |      |
+   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+   * |      |      | BL ON|  BRTG|  INC|   DEC|      |                    |      |      |      |      |      |      |      |
+   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+   * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
+   * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
+   * |      |      |      |      ||||||||      |      |      ||||||||      |      |      ||||||||      |      |      |      |
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   */
+  [_ADJUST] = LAYOUT(
+    KC_INT3, _______, _______, KC_MPLY, KC_WSCH, _______, _______,                        _______, _______, KC_BRIU, KC_BRID, _______, _______, _______,
+    KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
+    _______, KC_LNG1, KC_LNG2, _______, _______, _______, _______,                         KC_APP, _______, _______, _______, _______, _______, _______,
+    AS_ON,    AS_OFF,  AS_TOGG, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______, _______,
+    AS_DOWN,   AS_UP,  AS_RPT,  _______,          _______, _______, _______,      _______, _______, OSL(_ADJUST),      KC_HOME, KC_PGDN,  KC_END,  KC_END
+  )
+};
+
+// void persistent_default_layer_set(uint16_t default_layer) {
+  // eeconfig_update_default_layer(default_layer);
+  // default_layer_set(default_layer);
+// }
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // switch (keycode) {
+    // case QWERTY:
+      // if (record->event.pressed) {
+         // print("mode just switched to qwerty and this is a huge string\n");
+        // set_single_persistent_default_layer(_QWERTY);
+      // }
+      // return false;
+      // break;
+    // case LOWER:
+      // if (record->event.pressed) {
+        // layer_on(_LOWER);
+        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      // } else {
+        // layer_off(_LOWER);
+        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      // }
+      // return false;
+      // break;
+    // case RAISE:
+      // if (record->event.pressed) {
+        // layer_on(_RAISE);
+        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      // } else {
+        // layer_off(_RAISE);
+        // update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      // }
+      // return false;
+      // break;
+    // case ADJUST:
+      // if (record->event.pressed) {
+        // layer_on(_ADJUST);
+      // } else {
+        // layer_off(_ADJUST);
+      // }
+      // return false;
+      // break;
+  // }
+
+  #ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+  #endif
+
+  return true;
+}
+
