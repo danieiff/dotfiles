ba5f94893157c6192900df6e46a58d1db8656125
danieiff <sugielff@gmail.com>
2023-09-21 20:08:32 +0900

    feat(nvim): ✨lsp config for php

--- nvim/lua/misc_lang.lua
+++ nvim/lua/misc_lang.lua
@@ -25,6 +25,16 @@ local languages = {
       lintFormats = { '%l: %m: "%r"', '%l: %m', }
     }
   },
+  php = {
+    {
+      -- https://github.com/phpstan/phpstan https://github.com/nunomaduro/larastan
+      prefix = 'phpstan',
+      lintSource = 'phpstan',
+      lintCommand = './vendor/bin/phpstan analyse --no-progress --no-ansi --error-format=raw "${INPUT}"',
+      lintStdin = false,
+      lintFormats = { '%.%#:%l:%m' },
+      rootMarkers = { 'phpstan.neon', 'phpstan.neon.dist', 'composer.json' },
+    }
   }
 }
 local prettier = {
@@ -192,3 +202,34 @@ AUC('FileType', {
   end
 })
 
+AUC('FileType', {
+  pattern = 'php',
+  once = true,
+  callback = function()
+    require 'util'.ensure_npm_deps { 'intelephense' }
+    require 'lspconfig'.intelephense.setup {}
+
+    -- git clone https://github.com/xdebug/vscode-php-debug.git
+    -- cd vscode-php-debug
+    -- npm install && npm run build
+    dap.adapters.php = {
+      type = 'executable',
+      command = 'node',
+      args = { vim.fn.stdpath 'config' .. '/vscode-php-debug/out/phpDebug.js' }
+    }
+
+    dap.configurations.php = {
+      {
+        type = 'php',
+        request = 'launch',
+        name = 'Listen for Xdebug',
+        port = 9003,
+        pathMappings = {
+          ["/var/www/html"] = vim.fn.getcwd()
+        }
+      }
+    }
+
+    vim.schedule(vim.cmd.edit)
+  end
+})
