701a5253dad306e46518dbcf8ff61c435802e762
danieiff <sugielff@gmail.com>
2023-10-12 21:48:38 +0900

    refactor(init): ♻update setup script
    Gave up using homebrew cuz slow.

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,22 @@
-FROM ubuntu:20.04
+FROM ubuntu:22.04
 
 RUN apt update && \
-  apt-get update && \
-  apt install -y curl git tar unzip fzf gh yq clang
+  apt install -y curl git tar fzf ripgrep gh bat timg build-essential
 
 RUN apt install -y locales && \
   locale-gen ja_JP.UTF-8
 
-RUN curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash && \
-  nvim use 18
+RUN curl -o /usr/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 && \
+  chmod +x /usr/bin/yq
+
+RUN curl -fsSL https://github.com/sharkdp/fd/releases/download/v8.7.0/fd-v8.7.0-x86_64-unknown-linux-gnu.tar.gz | tar -xz && cp fd-v8.7.0-x86_64-unknown-linux-gnu/fd /usr/bin
+
+RUN curl -fsSL https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz | tar -C /usr/bin -xz
+
 RUN curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s lts && \
   npm install -g n
 
-RUN curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar xz && \
+RUN curl -fsSL https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar xz && \
   ln -fs /nvim-linux64/bin/nvim /usr/bin/nvim
 
 RUN git clone --depth 1 https://github.com/danieiff/dotfiles
@@ -21,4 +25,3 @@ RUN ln -fs /dotfiles/.bashrc ~/.bashrc && \
   mkdir -p ~/.config/nvim/lua && \
   ln -fs /dotfiles/nvim/init.lua ~/.config/nvim && \
   ln -fs /dotfiles/nvim/lua/* ~/.config/nvim/lua
-
--- init.sh
+++ init.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade && \
+  sudo apt install -y curl git tar fzf ripgrep gh bat timg clang
 
-shelltype="$([ -n "$BASH_VERSION" ] && echo "bash" || echo "zsh")"
-ln -fs "~/"$(dirname "${BASH_SOURCE[0]}")"/.${shelltype}rc" ~ && source "$HOME/.${shelltype}rc"
+shell="${BASH_VERSION:+bash}" || shell="zsh"
+ln -fs "~/"$(dirname "${BASH_SOURCE[0]}")"/.${shelltype}rc" ~ && source "$HOME/.${shell}rc"
 ln -fs ~/"$(dirname "${BASH_SOURCE[0]}")"/.gitconfig ~
 
+curl -L https://github.com/zellij-org/zellij/releases/download/v0.38.2/zellij-x86_64-unknown-linux-musl.tar.gz | tar -C /usr/local/bin -xz
+
+curl -L -o /usr/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 && chmod +x /usr/bin/yq
+
 # Node.js
-curl -L https://bit.ly/n-install | bash
+curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s lts && \
+  npm install -g n
 
 # NeoVim
-curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar xz
-ln -fs ~/nvim-linux64/bin/nvim /usr/bin/nvim
-mkdir -p ~/.config/nvim/lua
-ln -fs ~/dotfiles/init.lua ~/.config/nvim
-ln -fs ~/dotfiles/lua/* ~/.config/nvim/lua
+curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar xz && \
+  ln -fs /nvim-linux64/bin/nvim /usr/bin/nvim
+
+# Firebase
+npm i -g firebase-tools && sudo apt install default-jdk
 
 # WSL
+if [ "$WSLENV" ]; then
 WslLocalAppData="$(wslpath "$(powershell.exe \$Env:LocalAppData)" | tr -d "\r")"
-ln -s ~/dotfiles/windows-terminal.json "$WslLocalAppData/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
+ln -fs ~/dotfiles/windows-terminal.json "$WslLocalAppData/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
 
-## WSL SSH
+## SSH https://futurismo.biz/archives/6862/#-nat-
 sudo apt install openssh-server
-sudo systemctl start ssh
 ### Run in Powershell as Admin
 # $wsl_ipaddress1 = (wsl hostname -I).split(" ", 2)[0]
 # netsh interface portproxy delete v4tov4 listenaddress=0.0.0.0 listenport=22
@@ -30,20 +36,21 @@ sudo systemctl start ssh
 # netsh interface portproxy show v4tov4
 # Foreach ( $dir in "Inbound","Outbound" ) { New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort 22 -Action Allow -Protocol TCP }
 
+# sudo vi /etc/ssh/sshd_config # Edit yes/no for PubkeyAuthentication, PasswordAuthentication
 chmod 600 ~/.ssh/authorized_keys
 
+# ssh-keygen && ssh-copy-id <user@host>
+# # Generate public domain e.g.) https://www.noip.com/
+# # Config Wifi router to open port or proxy to different port from default of ssh
 # dev() {
-#   ssh -L "${1:-3000}:localhost:${1:-3000}" user@host
+#   ssh -L "${1:-3000}:localhost:${1:-3000}" <user@host>
 # }
 
+sudo systemctl start sshd
+
 ## Chrome (google-chrome)
-wget curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt install --fix-broken -y
-
-sudo apt install fzf gh yq clang
-
-cargo install ripgrep delta fd-find bat viu dotenv-linter
-
-# Firebase
-npm i -g firebase-tools && sudo apt install default-jdk
+sudo apt install --fix-all -y language-pack-ja fonts-ipafont fonts-ipaexfont
+fc-cache -fv
+fi
