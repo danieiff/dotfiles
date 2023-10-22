b107eb77bb9fb9e032cd84fb9067664123d93771
danieiff <sugielff@gmail.com>
2023-10-22 21:30:23 +0900

    refactor(init.sh): ♻update

--- init.sh
+++ init.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade && \
-  sudo apt install -y curl git tar fzf ripgrep gh bat timg clang
+sudo apt update && sudo apt install -y curl git tar fzf ripgrep gh bat clang
 
-shell="${BASH_VERSION:+bash}" || shell="zsh"
-ln -fs "~/"$(dirname "${BASH_SOURCE[0]}")"/.${shelltype}rc" ~ && source "$HOME/.${shell}rc"
-ln -fs ~/"$(dirname "${BASH_SOURCE[0]}")"/.gitconfig ~
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+git clone --depth 1 https://github.com/danieiff/dotfiles && \
+  cp -fsr /dotfiles ~/.config && cp -fs /dotfiles/.* ~  || true
 
 curl -L https://github.com/zellij-org/zellij/releases/download/v0.38.2/zellij-x86_64-unknown-linux-musl.tar.gz | tar -C /usr/local/bin -xz
 
-curl -L -o /usr/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 && chmod +x /usr/bin/yq
+curl -L -o /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 && chmod +x /usr/bin/yq
+
+curl -L -o /usr/local/bin/viu https://github.com/atanunq/viu/releases/latest/download/viu
 
 # Node.js
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash && \
@@ -19,40 +21,41 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash 
 
 # NeoVim
 curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar xz && \
-  ln -fs /nvim-linux64/bin/nvim /usr/bin/nvim
+  ln -fs /nvim-linux64/bin/nvim /usr/bin/nvim && \
+  nvim +q && nvim +TSUpdateSync +qa
 
 # Firebase
 npm i -g firebase-tools && sudo apt install default-jdk
 
 # WSL
 if [ "$WSLENV" ]; then
-WslLocalAppData="$(wslpath "$(powershell.exe \$Env:LocalAppData)" | tr -d "\r")"
-ln -fs ~/dotfiles/windows-terminal.json "$WslLocalAppData/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
+  WslLocalAppData="$(wslpath "$(powershell.exe \$Env:LocalAppData)" | tr -d "\r")"
+  ln -fs ~/dotfiles/windows-terminal-settings.json "$WslLocalAppData/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
 
-## SSH https://futurismo.biz/archives/6862/#-nat-
-sudo apt install openssh-server
-### Run in Powershell as Admin
-# $wsl_ipaddress1 = (wsl hostname -I).split(" ", 2)[0]
-# netsh interface portproxy delete v4tov4 listenaddress=0.0.0.0 listenport=22
-# netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=22 connectaddress=$wsl_ipaddress1 connectport=22
-# netsh interface portproxy show v4tov4
-# Foreach ( $dir in "Inbound","Outbound" ) { New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort 22 -Action Allow -Protocol TCP }
+  ## SSH https://futurismo.biz/archives/6862/#-nat-
+  sudo apt install openssh-server
+  ### Run in Powershell as Admin
+  # $wsl_ipaddress1 = (wsl hostname -I).split(" ", 2)[0]
+  # netsh interface portproxy delete v4tov4 listenaddress=0.0.0.0 listenport=22
+  # netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=22 connectaddress=$wsl_ipaddress1 connectport=22
+  # netsh interface portproxy show v4tov4
+  # Foreach ( $dir in "Inbound","Outbound" ) { New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort 22 -Action Allow -Protocol TCP }
 
-# sudo vi /etc/ssh/sshd_config # Edit yes/no for PubkeyAuthentication, PasswordAuthentication
-chmod 600 ~/.ssh/authorized_keys
+  # sudo vi /etc/ssh/sshd_config # Edit yes/no for PubkeyAuthentication, PasswordAuthentication
+  chmod 600 ~/.ssh/authorized_keys
 
-# ssh-keygen && ssh-copy-id <user@host>
-# # Opt) Generate public domain e.g.) https://www.noip.com/
-# # Config Wifi router to open port or proxy to different port from default of ssh
-# dev() {
-#   ssh -L "${1:-3000}:localhost:${1:-3000}" <user@host>
-# }
+  # ssh-keygen && ssh-copy-id <user@host>
+  # # Opt) Generate public domain e.g.) https://www.noip.com/
+  # # Config Wifi router to open port or proxy to different port from default of ssh
+  # dev() {
+  #   ssh -L "${1:-3000}:localhost:${1:-3000}" <user@host>
+  # }
 
-sudo systemctl start sshd
+  sudo systemctl start sshd
 
-## Chrome (google-chrome)
-curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt install --fix-all -y language-pack-ja fonts-ipafont fonts-ipaexfont
-fc-cache -fv
+  ## Chrome (google-chrome)
+  curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo dpkg -i google-chrome-stable_current_amd64.deb
+  sudo apt install --fix-all -y language-pack-ja fonts-ipafont fonts-ipaexfont
+  fc-cache -fv
 fi
