ec5de502ccc6427cc83eeebdb8de55b6503d23c3
danieiff <sugielff@gmail.com>
2023-10-24 22:45:50 +0900

    refactor(devcontainer): ♻vital modifications
    - Set SHELL as /bin/bash
    - Speed up docker startup by multiprocessing chmod
    - Use cloned entrypoint.sh instead of copy from host
    - Add 'nvim' to  ENTRYPOINT
    - Wait more on nvim init at docker build

--- devcontainer/entrypoint.sh
+++ devcontainer/entrypoint.sh
@@ -3,9 +3,12 @@
 USER_ID=${LOCAL_UID:-9001}
 GROUP_ID=${LOCAL_GID:-9001}
 
-echo "Starting with UID: $USER_ID, GID: $GROUP_ID"
-useradd -u "$USER_ID" -o -m user
-groupmod -g "$GROUP_ID" user
-export HOME=/home/user
+if [ ! -z "$USER_ID" ] && [ "$(id -u me)" != "$USER_ID" ]; then
+    groupadd --non-unique -g "$GROUP_ID" group
+    usermod --non-unique --uid "$USER_ID" --gid "$GROUP_ID" me
+fi
 
-exec /usr/sbin/gosu user "$@"
+find ~ -print0 | xargs -0 --max-args=1 --max-procs=10 chown -v me: 1> /dev/null
+chown me: /var/run/docker.sock
+
+exec /usr/sbin/gosu me "$@"
