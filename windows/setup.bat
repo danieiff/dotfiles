5dd24399873c7c0197cd4ef0c9ccfa5ece10beb2
danieiff <sugielff@gmail.com>
2023-12-11 06:15:48 +0900

    feat(nvim): setup for windows

---
+++ windows/setup.bat
@@ -0,0 +1,15 @@
+REM winget install --id Git.Git -e --source winget
+
+REM curl https://github.com/neovim/neovim/releases/download/nightly/nvim-win64.zip -O nvim-win64.zip
+REM Expand-Archive .\nvim-win64.zip
+
+REM git clone --depth 1 https://github.com/danieiff/dotfiles \
+REM New-Item -Path $ENV:LOCALAPPDATA/nvim -ItemType SymbolicLink -Value dotfiles/nvim
+
+REM npm install -g tree-sitter-cli
+
+REM curl https://github.com/jesseduffield/lazygit/releases/download/v0.40.2/lazygit_0.40.2_Windows_x86_64.zip -O lazygit.zip
+REM Expand-Archive .\lazygit.zip
+
+REM curl https://github.com/BurntSushi/ripgrep/releases/download/14.0.3/ripgrep-14.0.3-x86_64-pc-windows-gnu.zip -O ripgrep.zip
+REM Expand-Archive .\ripgrep.zip
