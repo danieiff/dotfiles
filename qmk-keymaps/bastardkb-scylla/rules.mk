f3c395db880ac28b575016a27f7a9102731f274e
danieiff <sugielff@gmail.com>
2024-02-03 15:03:10 +0900

    feat(keyboard): ✨new keymap for bastardkb/scylla

--- qmk-keymaps/bastardkb-scylla/rules.mk
+++ qmk-keymaps/bastardkb-scylla/rules.mk
@@ -1,4 +1,21 @@
-MOUSEKEY_ENABLE     = no      # Mouse keys
-NKRO_ENABLE         = yes     # USB Nkey Rollover
-RGB_MATRIX_ENABLE   = no      # Enable keyboard RGB matrix
-KEY_OVERRIDE_ENABLE = yes     # Enable Key Overrides, see https://docs.qmk.fm/#/feature_key_overrides
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = no
+AUDIO_ENABLE = no
+
+SPLIT_KEYBOARD = yes
+
+EXTRAKEY_ENABLE = yes # Audio control and System control
+MOUSEKEY_ENABLE = yes
+AUTO_SHIFT_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+DYNAMIC_TAPPING_TERM_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+
+# UNICODE_ENABLE = yes
+UNICODEMAP_ENABLE = no
+
+CONSOLE_ENABLE = no
+
+# VIA_ENABLE = yes
+# On first run, VIA copy the keymap from flash memory into EEPROM so that it can be rewritten by the VIA app. From this point QMK will use the keymap stored in EEPOM instead of flash.
+# To clear EEPROM, Hold Bootmagic Lite key(usually Escape) while plugging the board in, which will also place the board into bootloader mode; then unplug and replug the board.
