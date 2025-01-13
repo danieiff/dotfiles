658f43bac5a09814ea30c64d4287632743496843
danieiff <sugielff@gmail.com>
2025-01-13 23:14:49 +0900

    nvim: tabline, nav

---
+++ nvim/lua/navigation.lua
@@ -0,0 +1,105 @@
+K('<s-tab>', '<cmd>exe "tabp" .v:count1<cr>')
+K('<tab>', '<cmd>exe "tabn+" . v:count1<cr>')
+K('(', '<cmd>exe "tabp" .v:count1<cr>')
+K(')', '<cmd>exe "tabn+" . v:count1<cr>')
+AUC({ 'VimEnter', 'TabNew', 'TabClosed' }, {
+  callback = function()
+    for i, tab in ipairs(vim.api.nvim_list_tabpages()) do
+      K(i .. '<tab>', function() vim.api.nvim_set_current_tabpage(tab) end)
+    end
+  end
+})
+K('<leader><tab>', require 'fzf-lua'.tabs)
+
+require 'leap'.create_default_mappings()
+K('gS', require 'leap.remote'.action, { mode = { 'n', 'o' } })
+K('ga', require 'leap.treesitter'.select, { mode = { 'n', 'x', 'o' } })
+
+require 'navigate-note'.setup {}
+
+require 'fzf-lua'.setup {}
+K('<leader>f', function() require 'fzf-lua'.files { cwd = vim.fs.root(0, '.git') } end)
+K('<leader>F', require 'fzf-lua'.oldfiles)
+K('<leader>b', require 'fzf-lua'.buffers)
+K('<leader>m', require 'fzf-lua'.marks)
+K('<leader>j', require 'fzf-lua'.jumps)
+
+K('<leader>h', require 'fzf-lua'.helptags)
+K('<leader>M', require 'fzf-lua'.manpages)
+
+K('<leader>g', require 'fzf-lua'.live_grep)
+K('<leader>G', require 'fzf-lua'.live_grep_native)
+K('<leader>;', require 'fzf-lua'.live_grep_resume)
+K('<leader>:', require 'fzf-lua'.live_grep_glob)
+K('<leader>/', require 'fzf-lua'.grep)
+
+K('<C-g>', require 'fzf-lua'.grep_last)
+K('<C-g>', require 'fzf-lua'.grep_cword)
+K('<C-g>', require 'fzf-lua'.grep_cWORD)
+K('<C-g>', require 'fzf-lua'.grep_visual)
+K('<C-g>', require 'fzf-lua'.grep_project)
+K('<C-g>', require 'fzf-lua'.grep_curbuf)
+K('<C-g>', require 'fzf-lua'.grep_quickfix)
+K('<C-g>', require 'fzf-lua'.grep_loclist)
+K('<C-g>', require 'fzf-lua'.lgrep_curbuf)
+K('<C-g>', require 'fzf-lua'.lgrep_quickfix)
+K('<C-g>', require 'fzf-lua'.lgrep_loclist)
+
+K('<leader>q', require 'fzf-lua'.quickfix)
+K('<leader>Q', require 'fzf-lua'.quickfix_stack)
+K('<leader>l', require 'fzf-lua'.loclist)
+K('<leader>L', require 'fzf-lua'.loclist_stack)
+K('[q', '<cmd>cprevious<cr>')
+K(']q', '<cmd>cnext<cr>')
+K('[[q', '<cmd>cfirst<cr>')
+K(']]q', '<cmd>clast<cr>')
+K('[Q', '<cmd>colder<cr>')
+K(']Q', '<cmd>cnewer<cr>')
+K('qq', '<cmd>if empty(filter(getwininfo(), "v:val.quickfix")) | silent cw | else | cclose | endif<cr>')
+K('[l', '<cmd>lprevious<cr>')
+K(']l', '<cmd>lnext<cr>')
+K('[[l', '<cmd>lfirst<cr>')
+K(']]l', '<cmd>llast<cr>')
+K('[L', '<cmd>lolder<cr>')
+K(']L', '<cmd>lnewer<cr>')
+
+AUC('BufWinEnter', {
+  nested   = true,
+  callback = function(ev)
+    local root_dir = vim.fs.root(ev.buf, '.git')
+    if vim.bo[ev.buf].modifiable and vim.bo[ev.buf].ft and root_dir and root_dir ~= vim.fn.getcwd() then
+      vim.cmd.lcd(root_dir)
+    end
+  end
+})
+
+require 'nvim-tree'.setup {
+  sync_root_with_cwd = true,
+  respect_buf_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_root = {
+      enable = true
+    }
+  },
+  renderer = { indent_width = 1 },
+  actions = {
+    expand_all = { exclude = { '.git' } }
+  }
+}
+K('<C-t>', require 'nvim-tree.api'.tree.toggle, { silent = true })
+K('<leader>,', function()
+  vim.cmd.tabe(vim.fs.root(vim.fn.expand '$MYVIMRC', '.git'))
+  require 'nvim-tree.api'.tree.expand_all()
+end)
+
+K('g^', function() require 'treesitter-context'.go_to_context(vim.v.count1) end)
+
+require 'trouble'.setup {}
+require 'bqf'.setup {}
+
+require 'aerial'.setup {
+  filter_kind = false,
+  auto_jump = true
+}
+K('<C-s>', require 'aerial'.open)
--- nvim/lua/ui/tabline.lua
+++ nvim/lua/ui/tabline.lua
@@ -1,67 +1,66 @@
 local hl_tabline = vim.api.nvim_get_hl(0, { name = 'TabLine', link = false })
 local hl_tabline_sel = vim.api.nvim_get_hl(0, { name = 'TabLineSel', link = false })
+local hl_file_modified = vim.api.nvim_get_hl(0, { name = 'NvimTreeModifiedFileHL', link = false })
+vim.api.nvim_set_hl(0, 'TabLineFifth', { fg = hl_tabline.fg, bg = hl_tabline.bg, underdotted = true })
+vim.api.nvim_set_hl(0, 'TabLineFifthSel', { fg = hl_tabline_sel.fg, bg = hl_tabline_sel.bg, underdotted = true })
+vim.api.nvim_set_hl(0, 'TabLineModified', { fg = hl_file_modified.fg, bg = hl_tabline.bg })
+vim.api.nvim_set_hl(0, 'TabLineModifiedSel', { fg = hl_file_modified.fg, bg = hl_tabline_sel.bg })
+vim.api.nvim_set_hl(0, 'TabLineFifthModified', { fg = hl_tabline.fg, bg = hl_tabline.bg, underdotted = true })
+vim.api.nvim_set_hl(0, 'TabLineFifthModifiedSel',
+  { fg = hl_file_modified.fg, bg = hl_tabline_sel.bg, underdotted = true })
 
 local function get_icon_hl(bufnr)
-  local ft = vim.bo[bufnr].ft
-  local hl_icon, hl_icon_sel = ft .. 'Tab', ft .. 'TabSel'
+  local ft               = vim.bo[bufnr].ft
   local icon, icon_color = require 'nvim-web-devicons'.get_icon_color_by_filetype(ft)
+  local hl_icon          = ft .. 'Tab'
   if vim.tbl_isempty(vim.api.nvim_get_hl(0, { name = hl_icon, link = false })) then
     vim.api.nvim_set_hl(0, hl_icon, { fg = icon_color, bg = hl_tabline.bg })
+    vim.api.nvim_set_hl(0, hl_icon .. 'Sel', { fg = icon_color, bg = hl_tabline_sel.bg })
   end
-  if vim.tbl_isempty(vim.api.nvim_get_hl(0, { name = hl_icon_sel, link = false })) then
-    vim.api.nvim_set_hl(0, hl_icon_sel, { fg = icon_color, bg = hl_tabline_sel.bg })
-  end
-  return icon, hl_icon, hl_icon_sel
+  return icon, hl_icon
 end
 
 function _G.tabline()
   local maxwidth, width, shorter_width = vim.o.columns, 0, 0
   local tabs = {}
 
-  for _, tabnr in ipairs(vim.api.nvim_list_tabpages()) do
-    local bufnm_label, bufnm_label_shorter, hl, hl_sel = ' ', ' ', 'TabLine', 'TabLineSel'
-    local icon, hl_icon, hl_icon_sel = '', '', ''
+  for i, tabnr in ipairs(vim.api.nvim_list_tabpages()) do
+    local bufnm_label, bufnm_label_shorter, hl = ' ', ' ', 'TabLine' .. (i % 5 == 0 and 'Fifth' or '')
+    local icon, hl_icon                        = '', ''
 
     for _, win in ipairs({ vim.api.nvim_tabpage_get_win(tabnr), unpack(vim.api.nvim_tabpage_list_wins(tabnr)) }) do
       local bufnr = vim.api.nvim_win_get_buf(win)
+      if vim.bo[bufnr].modified and not hl:find 'Modified' then hl = hl .. 'Modified' end
+
       if CHECK_FILE_MODIFIABLE(bufnr, true) then
         local _tabnr, winnr = unpack(vim.fn.win_id2tabwin(win))
         bufnm_label = vim.fn.fnamemodify(vim.fn.bufname(bufnr), ':p:r:s?' .. vim.fn.getcwd(winnr, _tabnr) .. '/??')
         bufnm_label_shorter = vim.fn.pathshorten(bufnm_label, 1)
-        icon, hl_icon, hl_icon_sel = get_icon_hl(bufnr)
+        icon, hl_icon = get_icon_hl(bufnr)
         break
       elseif vim.bo[bufnr].ft == 'help' then
         bufnm_label = vim.fn.fnamemodify(vim.fn.bufname(bufnr), ':t:r')
         bufnm_label_shorter = bufnm_label
-        icon, hl_icon, hl_icon_sel = get_icon_hl(bufnr)
+        icon, hl_icon = get_icon_hl(bufnr)
       end
     end
 
     width = width + bufnm_label:len() + 3
     shorter_width = shorter_width + bufnm_label_shorter:len() + 3
 
-    table.insert(tabs,
-      {
-        hl = hl,
-        hl_sel = hl_sel,
-        nr = tabnr,
-        label = bufnm_label,
-        hl_icon = hl_icon,
-        hl_icon_sel = hl_icon_sel,
-        icon = icon
-      })
+    table.insert(tabs, { hl = hl, nr = tabnr, label = bufnm_label, hl_icon = hl_icon, icon = icon })
   end
 
   local is_shorter, is_shortest = maxwidth < width, maxwidth < shorter_width
   local selected_tabnr = vim.api.nvim_get_current_tabpage()
 
   return vim.fn.join(vim.tbl_map(function(tab)
-    local is_selected_tab = tab.nr == selected_tabnr
-    local hl_icon = is_selected_tab and tab.hl_icon_sel or tab.hl_icon
-    local hl = is_selected_tab and tab.hl_sel or tab.hl
+    local maybe_selected = tab.nr == selected_tabnr and 'Sel' or ''
     local label = is_shortest and vim.fn.fnamemodify(tab.label, ':t')
         or is_shorter and vim.fn.pathshorten(tab.label, 1)
         or tab.label
+    local hl = tab.hl .. maybe_selected
+    local hl_icon = tab.hl_icon .. maybe_selected
     return ('%%#%s#%%%sT%%#%s#%s%%*%%#%s# %s %%*'):format(hl, tab.nr, hl_icon, tab.icon, hl, label)
   end, tabs), '') .. '%#TabLineFill#%T'
 end
