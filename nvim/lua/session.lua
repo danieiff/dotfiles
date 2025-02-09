8e3924fc0aee16a9ab08fd0f686f4bcc14e34af3
danieiff <sugielff@gmail.com>
2025-02-10 01:17:59 +0900

    nvim: session config

--- nvim/lua/session.lua
+++ nvim/lua/session.lua
@@ -10,20 +10,71 @@ local function get_session_path(dir, branch)
       .. '.vim'
 end
 
+local function get_session_path_helper(selected_label)
+  return get_session_path(unpack(vim.split(selected_label, ' ')))
+end
+
+CMD('SelectSession', function(ev)
+  require 'fzf-lua'.fzf_exec(
+    'rg --files ' .. vim.fn.stdpath 'data' .. ' -g session*.vim',
+    {
+      prompt = 'Sessions> ',
+      fn_transform = function(line)
+        return vim.fn.fnamemodify(
+          line:gsub('^' .. vim.fn.stdpath 'data' .. '/session', ''):gsub('!', '/'):gsub(':', ' '):gsub(
+            '%.vim', ''),
+          ':~')
+      end,
+      preview = function(selected)
+        local filecontent = vim.fn.system { 'cat', get_session_path_helper(selected[1]) }
+        local result = '\n' .. vim.fn.system { 'ls', vim.fn.expand(selected[1]:match '%S+') }
+        for bufname in filecontent:gmatch 'badd %+%d+ (%S+)' do
+          result = bufname .. '\n' .. result
+        end
+        return result
+      end,
+      actions = {
+        ['default'] = function(selected)
+          vim.cmd.mksession(vim.v.this_session, { bang = true })
+          vim.cmd.source(get_session_path_helper(selected[1]))
+        end,
+        ['ctrl-e'] = function(selected) vim.cmd.tabedit(get_session_path_helper(selected[1])) end,
+        ['ctrl-y'] = function(selected) vim.fn.setreg(vim.v.register, get_session_path_helper(selected[1])) end,
+        ['ctrl-w'] = function(selected)
+          vim.cmd.mksession(get_session_path_helper(selected[1]), { bang = true })
+          require 'fzf-lua'.resume()
+        end,
+        ['ctrl-x'] = function(selected)
+          os.remove(get_session_path_helper(selected[1]))
+          require 'fzf-lua'.resume()
+        end
+      },
+      winopts = {
+        split = ev.bang and 'belowright vnew' or nil
+      }
+    })
+end, { bang = true })
+
+CMD('MkSession', 'mksession! ' .. get_session_path(), {})
+CMD('LoadSession', 'source ' .. get_session_path(), {})
+
 AUC('VimEnter', {
   nested = true,
   callback = function()
     local vim_argv = vim.fn.argv()
+
+    local cwd = vim.uv.cwd()
+    if cwd == vim.fn.expand '~' or cwd == vim.fn.fnamemodify('/', ':p') then
+      return vim.cmd 'e . | SelectSession!'
+    end
+
     local auc_id = AUC('VimLeave', { command = 'silent mksession! ' .. get_session_path() })
     if not pcall(vim.api.nvim_exec2, 'silent source ' .. get_session_path(), {}) then
       vim.api.nvim_del_autocmd(auc_id)
     end
+
     for _, path in ipairs(type(vim_argv) == 'table' and vim_argv or { vim_argv }) do
       vim.cmd.tabedit(path)
     end
   end
 })
-
-CMD('MkSession', 'mksession! ' .. get_session_path(), {})
-CMD('LoadSession', 'source ' .. get_session_path(), {})
-CMD('DeleteSession', function() os.remove(get_session_path()) end, {})
