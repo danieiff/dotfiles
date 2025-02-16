97b531fc3c8a82a2eccdc3e37d4412a35bda5fe2
danieiff <sugielff@gmail.com>
2025-02-16 19:36:03 +0900

    nvim: replace edgy.nvim with customized autocmd

--- nvim/lua/ui/window-layout.lua
+++ nvim/lua/ui/window-layout.lua
@@ -1,92 +1,159 @@
-local edgy_opts = {
-  top = {
-    { ft = "man",          filter = function(_, win) return not vim.w[win].fzf_lua_preview end },
-    { ft = 'OverseerList', size = { width = 0.2, height = 0.5 } },
-    { ft = '',             filter = function(buf) return vim.b[buf].overseer_task end },
+local ft_layouts = {
+  man = {
+    pos = 'top', filter = function(_, win) return not vim.w[win].fzf_lua_preview end
   },
-  bottom = {
-    'iron',
-    'Trouble',
-    'qf',
-    { ft = "neotest-output-panel", size = { height = 15 } }
+  OverseerList = {
+    pos = 'top', width = 0.2, height = 0.5,
   },
-  left = {
-    'Avante',
-    'AvanteInput',
-    'neotest-summary',
-    'undotree',
-    { ft = 'diff', filter = function(buf) return vim.b[buf].isUndotreeBuffer end },
-    {
-      ft = "help",
-      size = { width = 80 },
-      filter = function(bufnr)
-        if vim.fn.bufname(bufnr) == '_FzfLuaHelp' then return false end
+  overseer_task = {
+    pos = 'top'
+  },
+  iron = {
+    pos = 'bottom'
+  },
+  Trouble = {
+    pos = 'bottom',
+  },
+  qf = {
+    pos = 'bottom',
+  },
+  ['neotest-output-panel'] = {
+    pos = 'bottom', height = 15
+  },
+  Avante = {
+    pos = 'left'
+  },
+  AvanteInput = {
+    pos = 'left'
+  },
+  ['neotest-summary'] = {
+    pos = 'left'
+  },
+  undotree = {
+    pos = 'left'
+  },
+  diff = {
+    pos = 'left', filter = function(buf) return vim.b[buf].isUndotreeBuffer end
+  },
+  help = {
+    pos = 'left',
+    width = 80,
+    filter = function(buf)
+      if vim.fn.bufname(buf) == '_FzfLuaHelp' then return false end
 
-        local is_only_win = #vim.tbl_filter(function(win)
-          return CHECK_FILE_MODIFIABLE(vim.api.nvim_win_get_buf(win))
-        end, vim.api.nvim_tabpage_list_wins(0)) <= 1
+      local is_only_win = #vim.tbl_filter(function(win)
+        return CHECK_FILE_MODIFIABLE(vim.api.nvim_win_get_buf(win))
+      end, vim.api.nvim_tabpage_list_wins(0)) <= 1
 
-        return not is_only_win
-      end
-    },
+      return not is_only_win
+    end
   },
-  right = {
-    { ft = 'aerial',   pinned = true,         open = require 'aerial'.toggle },
-    {
-      ft = 'NvimTree',
-      pinned = true,
-      open = require 'nvim-tree.api'.tree.toggle,
-      filter = function()
-        local is_only_win = #vim.tbl_filter(function(win)
-          return CHECK_FILE_MODIFIABLE(vim.api.nvim_win_get_buf(win))
-        end, vim.api.nvim_tabpage_list_wins(0)) <= 1
-        return not is_only_win
-      end
-    },
-    { ft = 'grug-far', size = { width = 0.4 } },
+  aerial = {
+    pos = 'right', open = require 'aerial'.toggle
   },
-  options = {
-    left = { size = 0.2 },
-    right = { size = 0.2 },
-    top = { size = 0.5 },
+  NvimTree = {
+    pos = 'right',
+    open = require 'nvim-tree.api'.tree.toggle,
+    filter = function()
+      local is_only_win = #vim.tbl_filter(function(win)
+        return CHECK_FILE_MODIFIABLE(vim.api.nvim_win_get_buf(win))
+      end, vim.api.nvim_tabpage_list_wins(0)) < 1
+      return not is_only_win
+    end
   },
-  animate = {
-    enabled = false,
+  ['grug-far'] = {
+    pos = 'right', width = 0.4
   },
-  wo = {
-    winbar = false
-  },
-  keys = {
-    ['<right>'] = function(win) win:resize('width', 2) end,
-    ['<left>'] = function(win) win:resize('width', -2) end,
-    ['<up>'] = function(win) win:resize('height', 2) end,
-    ['<down>'] = function(win) win:resize('height', -2) end,
-  },
+  trouble = {
+    pos = function(w) return vim.w[w].trouble.position end,
+    filter = function(_, w)
+      return vim.w[w].trouble.type == 'split' and vim.w[w].trouble.relative == 'editor' and
+          not vim.w[w].trouble_preview
+    end
+  }
 }
 
-for _, pos in ipairs { 'top', 'bottom', 'left', 'right' } do
-  edgy_opts[pos] = edgy_opts[pos] or {}
-  table.insert(edgy_opts[pos], {
-    ft = 'trouble',
-    filter = function(_, win)
-      return vim.w[win].trouble
-          and vim.w[win].trouble.position == pos
-          and vim.w[win].trouble.type == "split"
-          and vim.w[win].trouble.relative == "editor"
-          and not vim.w[win].trouble_preview
-    end,
-  })
-end
+LAYOUTS = {}
 
-require 'edgy'.setup(edgy_opts)
-K(';;', require 'edgy'.toggle)
+AUC({ 'BufWinEnter', 'WinResized', 'FileType' }, {
+  desc = 'layout side windows',
+  group = vim.api.nvim_create_augroup('SideWindowLayout', { clear = true }),
+  callback = function(ev)
+    if vim.b[ev.buf].overseer_task then
+      vim.b[ev.buf].filetype = 'overseer_task'
+    end
+    if vim.v.exiting ~= vim.NIL or vim.fn.getcmdwintype() ~= "" or vim.bo[ev.buf].ft == '' then
+      return
+    end
+
+    local current_tabpage = vim.api.nvim_get_current_tabpage()
+    LAYOUTS[current_tabpage] = LAYOUTS[current_tabpage] or { top = {}, bottom = {}, left = {}, right = {} }
+
+    local wins = vim.api.nvim_tabpage_list_wins(current_tabpage)
+
+    for _, win in ipairs(wins) do
+      local buf = vim.api.nvim_win_get_buf(win)
+      local layout = ft_layouts[vim.bo[buf].filetype]
+      if layout and not vim.w[win].side_layout and (type(layout.filter) ~= 'function' or layout.filter(buf, win)) then
+        local pos = type(layout.pos) == 'function' and layout.pos() or layout.pos
+
+        local layout_success = true
+        local last_side = LAYOUTS[current_tabpage][pos][#LAYOUTS[current_tabpage][pos]]
+        if not last_side then
+          vim.api.nvim_win_call(win,
+            function() vim.cmd('wincmd ' .. ({ left = 'H', right = 'L', top = 'K', bottom = 'J' })[pos]) end)
+        else
+          layout_success = pcall(vim.fn.win_splitmove, win, last_side.win,
+            { vertical = pos == 'top' or pos == 'bottom', rightbelow = true })
+        end
+        if layout_success then
+          table.insert(LAYOUTS[current_tabpage][pos], { win = win, width = layout.width, height = layout.height })
+          vim.w[win].side_layout = pos
+        else
+          vim.notify('Failed to layout ' .. win .. ' next to ' .. last_side.win, vim.log.levels.WARN)
+        end
+      end
+    end
+
+    for pos, layouts in pairs(LAYOUTS[current_tabpage]) do
+      local is_vertical             = pos == 'top' or pos == 'bottom'
+      local main_size               = pos == 'top' and 0.5 or 0.2
+      main_size                     = math.ceil(main_size > 1 and main_size or
+        ((is_vertical and (vim.o.lines - 2) or vim.o.columns) * main_size))
+      local main_size_key           = is_vertical and 'height' or 'width'
+      local cross_size_key          = is_vertical and 'width' or 'height'
+      local available_cross_size    =
+          (is_vertical and vim.o.columns or (vim.o.lines - 2)) - (#LAYOUTS[current_tabpage][pos] - 1)
+      local rest_distribution_cnt   = #LAYOUTS[current_tabpage][pos]
+
+      LAYOUTS[current_tabpage][pos] = vim.tbl_filter(function(w)
+        if vim.api.nvim_win_is_valid(w.win) then
+          main_size = math.max(main_size, w[main_size_key] or 0)
+          available_cross_size = available_cross_size - (w[cross_size_key] or 0)
+          rest_distribution_cnt = rest_distribution_cnt - (w[cross_size_key] and 1 or 0)
+          return true
+        end
+        return false
+      end, layouts)
+
+      local rest_cross_size         = rest_distribution_cnt == 0 and 0 or
+          math.ceil(available_cross_size / rest_distribution_cnt)
+
+      for i, w in ipairs(LAYOUTS[current_tabpage][pos]) do
+        if i == 1 then
+          vim.api['nvim_win_set_' .. main_size_key](w.win, main_size)
+        end
+        vim.api['nvim_win_set_' .. cross_size_key](w.win, w[cross_size_key] or rest_cross_size)
+      end
+    end
+  end
+})
 
 AUC('WinEnter', {
+  desc = 'auto resize windows',
+  group = vim.api.nvim_create_augroup('MainWindowLayout', { clear = true }),
   nested = true,
-  desc = 'auto resize non-edgy windows',
   callback = function(ev)
-    if vim.b[ev.buf].edgy_keys or vim.api.nvim_win_get_config(0).relative ~= '' then return end
-
     local editor_width, editor_height = vim.o.columns, vim.o.lines - 2
 
     local current_win = vim.api.nvim_get_current_win()
@@ -94,41 +161,42 @@ AUC('WinEnter', {
     local current_win_line_end = current_win_line_start + vim.api.nvim_win_get_height(current_win)
     local current_win_col_end = current_win_col_start + vim.api.nvim_win_get_width(current_win)
 
-    local edgy_sizes = { top = 0, bottom = 0, left = 0, right = 0 }
     local horizontal_splits, vertical_splits = {}, {}
+
+    if vim.api.nvim_win_get_config(0).relative ~= '' or vim.w[current_win].side_layout or ft_layouts[vim.bo[ev.buf].ft] then
+      return
+    end
 
     for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
       local win_cfg = vim.api.nvim_win_get_config(win)
-      if win_cfg.relative == '' then
-        local edgy_win = require 'edgy'.get_win(win)
+      if win_cfg.relative == '' and not vim.w[win].side_layout and not ft_layouts[vim.bo[vim.api.nvim_win_get_buf(win)].ft] then
+        local win_line_start, win_col_start = unpack(vim.api.nvim_win_get_position(win))
+        local win_line_end, win_col_end = win_line_start + win_cfg.height, win_col_start + win_cfg.width
 
-        if edgy_win and edgy_sizes[edgy_win.view.edgebar.pos] == 0 then
-          edgy_sizes[edgy_win.view.edgebar.pos] =
-              (edgy_win.view.edgebar.vertical and edgy_win.width or edgy_win.height) + 1
-        else
-          local win_line_start, win_col_start = unpack(vim.api.nvim_win_get_position(win))
-          local win_line_end, win_col_end = win_line_start + win_cfg.height, win_col_start + win_cfg.width
-
-          if win_cfg.height < editor_height then
-            if not (win_col_end < current_win_col_start or current_win_col_end < win_col_start) then
-              table.insert(horizontal_splits, win)
-            end
+        if win_cfg.height < editor_height then
+          if not (win_col_end < current_win_col_start or current_win_col_end < win_col_start) then
+            table.insert(horizontal_splits, win)
           end
-          if win_cfg.width < editor_width then
-            if not (win_line_end < current_win_line_start or current_win_line_end < win_line_start) then
-              table.insert(vertical_splits, win)
-            end
+        end
+        if win_cfg.width < editor_width then
+          if not (win_line_end < current_win_line_start or current_win_line_end < win_line_start) then
+            table.insert(vertical_splits, win)
           end
         end
       end
     end
 
+    local side_size_top = vim.tbl_get(vim.t, 'side_sizes', 'top') or 0
+    local side_size_bottom = vim.tbl_get(vim.t, 'side_sizes', 'bottom') or 0
+    local side_size_left = vim.tbl_get(vim.t, 'side_sizes', 'left') or 0
+    local side_size_right = vim.tbl_get(vim.t, 'side_sizes', 'right') or 0
+
     local other_horizontal_splits_cnt = #horizontal_splits - 1
     if other_horizontal_splits_cnt > 0 then
-      local non_edgy_height = editor_height - other_horizontal_splits_cnt - edgy_sizes.top - edgy_sizes.bottom
-      local new_current_win_height = math.floor(non_edgy_height * 2 / 3)
-      local rest_distributed_height = math.floor((non_edgy_height - new_current_win_height) / other_horizontal_splits_cnt)
-      local rest_distributed_remainder_height = (non_edgy_height - new_current_win_height) % other_horizontal_splits_cnt
+      local main_height = editor_height - other_horizontal_splits_cnt - side_size_top - side_size_bottom
+      local new_current_win_height = math.floor(main_height * 2 / 3)
+      local rest_distributed_height = math.floor((main_height - new_current_win_height) / other_horizontal_splits_cnt)
+      local rest_distributed_remainder_height = (main_height - new_current_win_height) % other_horizontal_splits_cnt
       new_current_win_height = new_current_win_height + rest_distributed_remainder_height
 
       for _, win in ipairs(horizontal_splits) do
@@ -138,10 +206,10 @@ AUC('WinEnter', {
 
     local other_vertical_splits_cnt = #vertical_splits - 1
     if other_vertical_splits_cnt > 0 then
-      local non_edgy_width = editor_width - other_vertical_splits_cnt - edgy_sizes.left - edgy_sizes.right
-      local new_current_win_width = math.floor(non_edgy_width * 2 / 3)
-      local rest_distributed_width = math.floor((non_edgy_width - new_current_win_width) / other_vertical_splits_cnt)
-      local rest_distributed_remainder_width = (non_edgy_width - new_current_win_width) % other_vertical_splits_cnt
+      local main_width = editor_width - other_vertical_splits_cnt - side_size_left - side_size_right
+      local new_current_win_width = math.floor(main_width * 2 / 3)
+      local rest_distributed_width = math.floor((main_width - new_current_win_width) / other_vertical_splits_cnt)
+      local rest_distributed_remainder_width = (main_width - new_current_win_width) % other_vertical_splits_cnt
       new_current_win_width = new_current_win_width + rest_distributed_remainder_width
 
       for _, win in ipairs(vertical_splits) do
